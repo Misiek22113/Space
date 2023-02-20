57cf32eef84dde22077227e70844258fbdaa2fb3
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-02-20 23:26:41 +0100

    add background, button, home page info

---
+++ SpaceProject/src/components/HomeInfo/HomeInfo.tsx
@@ -0,0 +1,17 @@
+import "./HomeInfo.scss";
+
+const HomeInfo = () => {
+  return (
+    <div>
+      <h3>SO, U WANT TO LEARN ABOUT</h3>
+      <h1>SPACE</h1>
+      <h4>
+        If so you are on perfect website. Here you can learn basics of our solar
+        system. How it looks like, what planets are included and how big and far
+        from Earth they are. If you are read to start, let`s explore!
+      </h4>
+    </div>
+  );
+};
+
+export default HomeInfo;
