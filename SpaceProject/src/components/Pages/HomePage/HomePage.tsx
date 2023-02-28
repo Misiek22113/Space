f299dcd9812186236f1d9344b1c47ff2ff995612
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-02-28 21:44:28 +0100

    set up framer motion, add some animations

--- SpaceProject/src/components/Pages/HomePage/HomePage.tsx
+++ SpaceProject/src/components/Pages/HomePage/HomePage.tsx
@@ -2,16 +2,35 @@ import HomeInfo from "../../HomeInfo/HomeInfo";
 import "./HomePage.scss";
 import Button from "../../../styles/button.module.scss";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const HomePage = () => {
   return (
     <div className="background">
       <div className="home-info-section">
-        <HomeInfo />
+        <motion.div
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 0.8 }}
+        >
+          <HomeInfo />
+        </motion.div>
       </div>
-      <Link to="/SolarSystem">
-        <button className={`${Button.circle}`}>EXPLORE</button>
-      </Link>
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeOut", duration: 0.8 }}
+      >
+        <Link to="/SolarSystem">
+          <motion.button
+            className={`${Button.circle}`}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            EXPLORE
+          </motion.button>
+        </Link>
+      </motion.div>
     </div>
   );
 };
