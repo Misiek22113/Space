e73d8717bd3a8e1a1b417a3dc5dd477eb80eee6f
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-03-01 20:18:35 +0100

    change homePage components font size

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -6,32 +6,35 @@ import { motion } from "framer-motion";
 
 const HomePage = () => {
   return (
-    <div className="background">
-      <div className="home-info-section">
+    <>
+      <div className="background"></div>
+      <div className="home-page-content">
+        <div className="home-info-section">
+          <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ ease: "easeOut", duration: 0.8 }}
+          >
+            <HomeInfo />
+          </motion.div>
+        </div>
         <motion.div
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ ease: "easeOut", duration: 0.8 }}
         >
-          <HomeInfo />
+          <Link to="/SolarSystem">
+            <motion.button
+              className={`${Button.circle}`}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              EXPLORE
+            </motion.button>
+          </Link>
         </motion.div>
       </div>
-      <motion.div
-        initial={{ y: 20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ ease: "easeOut", duration: 0.8 }}
-      >
-        <Link to="/SolarSystem">
-          <motion.button
-            className={`${Button.circle}`}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            EXPLORE
-          </motion.button>
-        </Link>
-      </motion.div>
-    </div>
+    </>
   );
 };
 
