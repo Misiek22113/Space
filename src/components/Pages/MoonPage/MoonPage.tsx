5927b9aba70e412b2ba6262e77825089f0b0778e
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-03-03 16:50:58 +0100

    add moon page & solar system page animations

--- src/components/Pages/MoonPage/MoonPage.tsx
+++ src/components/Pages/MoonPage/MoonPage.tsx
@@ -1,11 +1,18 @@
 import Card from "../../Card/Card";
 import "./MoonPage.scss";
 import MOON from "./MoonInfo";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const MoonPage = () => {
   return (
     <div className="background-moon">
-      <div className="card-section">
+      <motion.div
+        className="card-section"
+        initial={{ y: 30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "circOut", duration: 0.6 }}
+      >
         <Card
           planet={MOON.planet}
           overview={MOON.overview}
@@ -15,7 +22,7 @@ const MoonPage = () => {
           planetImgPath={MOON.planetImgPath}
           onRight={true}
         />
-      </div>
+      </motion.div>
 
       {/* <h1>MoonPage</h1> */}
     </div>
--- src/components/Pages/SolarSystem/SolarSystem.tsx
+++ src/components/Pages/SolarSystem/SolarSystem.tsx
@@ -12,13 +12,20 @@ import {
   NEPTUNE,
 } from "./PlanetsInfo";
 import "./SolarSystem.scss";
+import { motion } from "framer-motion";
 
 const SolarSystem = () => {
   return (
     <>
       <div className="solar-system-background"></div>
       <div className="planet-section">
-        <div className="sun-card">
+        <motion.div
+          className="sun-card"
+          initial={{ x: 30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
           <Card
             planet={SUN.planet}
             overview={SUN.overview}
@@ -28,26 +35,46 @@ const SolarSystem = () => {
             planetImgPath={SUN.planetImgPath}
             onRight={true}
           ></Card>
-        </div>
-        <Card
-          planet={"MERUCRY"}
-          overview={MERCURY.overview}
-          distance={MERCURY.distance}
-          moons={MERCURY.moons}
-          years={MERCURY.years}
-          planetImgPath={"Mercury.png"}
-          onRight={false}
-        ></Card>
-        <Card
-          planet={VENUS.planet}
-          overview={VENUS.overview}
-          distance={VENUS.distance}
-          moons={VENUS.moons}
-          years={VENUS.years}
-          planetImgPath={"venus.png"}
-          onRight={true}
-        ></Card>
-        <div className="earth-section">
+        </motion.div>
+        <motion.div
+          initial={{ x: -30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <Card
+            planet={"MERUCRY"}
+            overview={MERCURY.overview}
+            distance={MERCURY.distance}
+            moons={MERCURY.moons}
+            years={MERCURY.years}
+            planetImgPath={"Mercury.png"}
+            onRight={false}
+          ></Card>
+        </motion.div>
+        <motion.div
+          initial={{ x: 30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <Card
+            planet={VENUS.planet}
+            overview={VENUS.overview}
+            distance={VENUS.distance}
+            moons={VENUS.moons}
+            years={VENUS.years}
+            planetImgPath={"venus.png"}
+            onRight={true}
+          ></Card>
+        </motion.div>
+        <motion.div
+          className="earth-section"
+          initial={{ x: -30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
           <div>
             <Link to="/moon">
               <img
@@ -65,53 +92,87 @@ const SolarSystem = () => {
             planetImgPath={"Earth_small.png"}
             onRight={false}
           ></Card>
-        </div>
-
-        <Card
-          planet={"MARS"}
-          overview={MARS.overview}
-          distance={MARS.distance}
-          moons={MARS.moons}
-          years={MARS.years}
-          planetImgPath={"Mars_small.png"}
-          onRight={true}
-        ></Card>
-        <Card
-          planet={"JUPITER"}
-          overview={JUPITER.overview}
-          distance={JUPITER.distance}
-          moons={JUPITER.moons}
-          years={JUPITER.years}
-          planetImgPath={"Jupiter_small.png"}
-          onRight={false}
-        ></Card>
-        <Card
-          planet={"SATURN"}
-          overview={SATURN.overview}
-          distance={SATURN.distance}
-          moons={SATURN.moons}
-          years={SATURN.years}
-          planetImgPath={"Saturn_without_ring.png"}
-          onRight={true}
-        ></Card>
-        <Card
-          planet={"URANUS"}
-          overview={URANUS.overview}
-          distance={URANUS.distance}
-          moons={URANUS.moons}
-          years={URANUS.years}
-          planetImgPath={"Uran_small.png"}
-          onRight={false}
-        ></Card>
-        <Card
-          planet={"NEPTUNE"}
-          overview={NEPTUNE.overview}
-          distance={NEPTUNE.distance}
-          moons={NEPTUNE.moons}
-          years={NEPTUNE.years}
-          planetImgPath={"Neptun_small.png"}
-          onRight={true}
-        ></Card>
+        </motion.div>
+        <motion.div
+          initial={{ x: 30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <Card
+            planet={"MARS"}
+            overview={MARS.overview}
+            distance={MARS.distance}
+            moons={MARS.moons}
+            years={MARS.years}
+            planetImgPath={"Mars_small.png"}
+            onRight={true}
+          ></Card>
+        </motion.div>
+        <motion.div
+          initial={{ x: -30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <Card
+            planet={"JUPITER"}
+            overview={JUPITER.overview}
+            distance={JUPITER.distance}
+            moons={JUPITER.moons}
+            years={JUPITER.years}
+            planetImgPath={"Jupiter_small.png"}
+            onRight={false}
+          ></Card>
+        </motion.div>
+        <motion.div
+          initial={{ x: 30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <Card
+            planet={"SATURN"}
+            overview={SATURN.overview}
+            distance={SATURN.distance}
+            moons={SATURN.moons}
+            years={SATURN.years}
+            planetImgPath={"Saturn_without_ring.png"}
+            onRight={true}
+          ></Card>
+        </motion.div>
+        <motion.div
+          initial={{ x: -30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <Card
+            planet={"URANUS"}
+            overview={URANUS.overview}
+            distance={URANUS.distance}
+            moons={URANUS.moons}
+            years={URANUS.years}
+            planetImgPath={"Uran_small.png"}
+            onRight={false}
+          ></Card>
+        </motion.div>
+        <motion.div
+          initial={{ x: 30, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ ease: "circOut", duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <Card
+            planet={"NEPTUNE"}
+            overview={NEPTUNE.overview}
+            distance={NEPTUNE.distance}
+            moons={NEPTUNE.moons}
+            years={NEPTUNE.years}
+            planetImgPath={"Neptun_small.png"}
+            onRight={true}
+          ></Card>
+        </motion.div>
       </div>
     </>
   );
